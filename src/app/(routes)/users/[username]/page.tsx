57d62ec4186126d082a53a0febd8a0f74529e82b
Muhib <syedmuhibfarooq@gmail.com>
2025-05-28 17:19:50 +0500

    Fix: correct typing for UserProfilePage

--- src/app/(routes)/users/[username]/page.tsx
+++ src/app/(routes)/users/[username]/page.tsx
@@ -1,26 +1,31 @@
-import {getSessionEmail} from "@/action";
+import { getSessionEmail } from "@/action";
 import ProfilePageContent from "@/Components/ProfilePageContent";
-import {prisma} from "@/db";
+import { prisma } from "@/db";
 
-export default async function UserProfilePage({
-  params:{username},
-}:{
-  params:{username:string};
-}) {
+interface UserProfilePageProps {
+  params: {
+    username: string;
+  };
+}
+
+export default async function UserProfilePage({ params }: UserProfilePageProps) {
   const sessionEmail = await getSessionEmail() || '';
   const profile = await prisma.profile.findFirstOrThrow({
-    where:{username:username}
+    where: { username: params.username },
   });
+
   const ourFollow = await prisma.follower.findFirst({
     where: {
       followingProfileEmail: sessionEmail,
       followedProfileId: profile.id,
     },
   });
+
   return (
     <ProfilePageContent
       isOurProfile={profile.email === sessionEmail}
       ourFollow={ourFollow}
-      profile={profile} />
+      profile={profile}
+    />
   );
 }
